7dfa9d152446012acfaf660851a2a0d5a6ff5f98
ekarkat <ekarkat@gmail.com>
2024-04-11 17:19:36 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,9 +7,17 @@ from typing import List
 import logging
 
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Hide infos"""
+    for field in fields:
+        pattern = f"{field}=[^{separator}]*"
+        message = re.sub(pattern, f"{field}={redaction}", message)
+    return message
+
+
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -20,12 +28,3 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Hide infos"""
-    for field in fields:
-        pattern = f"{field}=[^{separator}]*"
-        message = re.sub(pattern, f"{field}={redaction}", message)
-    return message
