a816e9f3c7f409a6f26a731e0b222e5c540cb7d0
ekarkat <ekarkat@gmail.com>
2024-04-16 22:53:00 +0100

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Class Basic Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extrct base """
+        at = authorization_header
+        if not at or not isinstance(at, str) or not at.startswith('Basic '):
+            return None
+        else:
+            return authorization_header[6:]
