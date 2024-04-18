4b25008d177b923f99cd1c1b52012d8623c9b409
ekarkat <ekarkat@gmail.com>
2024-04-18 22:13:11 +0100

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,6 +11,7 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """ Class Basic Auth"""
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID """
         if not user_id or not isinstance(user_id, str):
@@ -18,3 +19,8 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
