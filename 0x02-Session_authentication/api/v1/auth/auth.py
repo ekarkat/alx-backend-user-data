4a0dba463109e09a5a85d9f48d0aa3480b049a5a
ekarkat <ekarkat@gmail.com>
2024-04-18 21:55:39 +0100

    task 0 start

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Auth class """
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """ Class Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Documentation for method """
+        if not excluded_paths or not path:
+            return True
+        if path in excluded_paths or (path + '/') in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Documentation for method """
+        if request is None:
+            return None
+        header = request.headers.get("Authorization")
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Documentation for method """
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+""" Basic Auth class """
+from typing import List, TypeVar
+from flask import request
+from api.v1.auth.auth import Auth
+import base64
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ Class Basic Auth"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extrct base """
+        at = authorization_header
+        if not at or not isinstance(at, str) or not at.startswith('Basic '):
+            return None
+        else:
+            return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, b64_auth_head: str) -> str:
+        """ Encode """
+        b64 = b64_auth_head
+        if not b64 or not isinstance(b64, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(b64)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return(decoded_string)
+        except Exception:
+            return None
+
+    def extract_user_credentials(self, d_b64: str) -> (str, str):
+        """ Extract """
+        if d_b64 is None or not isinstance(d_b64, str) or ':' not in d_b64:
+            return (None, None)
+        return d_b64.split(':', 1)
+
+    def user_object_from_credentials(self, u_m: str,
+                                     u_pw: str) -> TypeVar('User'):
+        """ user_object_from_credentials
+            u_m = user email
+            u_p = user password
+        """
+        if not all((u_m, isinstance(u_m, str), u_pw, isinstance(u_pw, str))):
+            return None
+        try:
+            users = User.search({'email': u_m})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(u_pw):
+                return user
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Overload current_user """
+        header_auth = self.authorization_header(request)
+        if not header_auth:
+            return None
+        b64 = self.extract_base64_authorization_header(header_auth)
+        de_b64 = self.decode_base64_authorization_header(b64)
+        u_info = self.extract_user_credentials(de_b64)
+        u_cred = self.user_object_from_credentials(u_info[0], u_info[1])
+        return u_cred
