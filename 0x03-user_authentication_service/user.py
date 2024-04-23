9d1bc01fb6989ab916226f6770576ee4c9166d6a
ekarkat <ekarkat@gmail.com>
2024-04-23 21:29:03 +0100

    python3 venv :'(

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ The User module """
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,6 +14,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
