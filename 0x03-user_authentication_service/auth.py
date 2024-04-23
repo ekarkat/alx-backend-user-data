23f986a293e0f65ec290f5f163666c468b8633f3
ekarkat <ekarkat@gmail.com>
2024-04-23 23:11:11 +0100

    task 4 hash a pass

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+"""Auth Module"""
+import bcrypt
+
+
+def _hash_password(password: str) -> str:
+    """Hash a password"""
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
