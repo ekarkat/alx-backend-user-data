9c1d8f78aee7b0b4f026c0db5cbe52536cdf0dd6
ekarkat <ekarkat@gmail.com>
2024-04-23 23:43:23 +0100

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""FLASK APP module"""
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """Simple Route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
