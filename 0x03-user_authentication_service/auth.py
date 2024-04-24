cef112977404ab5a88ae376d83504802290ac664
ekarkat <ekarkat@gmail.com>
2024-04-24 23:49:54 +0100

    tas 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,11 @@ class Auth:
             hashed_pwd = _hash_password(password)
             new_user = self._db.add_user(email, hashed_pwd)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate usr login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except Exception:
+            return False
